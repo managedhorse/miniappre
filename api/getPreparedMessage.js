825218f43ed7aeed7790b0b54af5ca4424a393ca
managedhorse <david.webpersonal@gmail.com>
2025-01-05 15:21:24 +0800

    Update image paths to fix deployment issues

--- api/getPreparedMessage.js
+++ api/getPreparedMessage.js
@@ -3,6 +3,18 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
+  // For a serverless function on Vercel using CommonJS:
+  res.setHeader('Access-Control-Allow-Origin', 'https://myfront.vercel.app'); 
+  // or '*' if you really want to allow from anywhere
+
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS'); 
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    // CORS preflight
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }

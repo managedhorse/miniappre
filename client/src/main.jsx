ca0312a182f295832bef239accdda48666116221
managedhorse <david.webpersonal@gmail.com>
2025-01-24 21:06:03 +0800

    bug fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
 } from "react-router-dom";
 import { UserProvider } from "./context/userContext.jsx"; // Import the UserProvider
@@ -19,7 +19,7 @@ import LuckyWhile from "./Components/LuckyWhile.jsx";
 import DailyReward from "./Components/DailyReward.jsx";
 import Plinko from "./Components/plinko.jsx";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
